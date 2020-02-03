f6472c1f13f8ef1a0da347eb4d193cffa1a92a97
zouhang <zouhang@didiglobal.com>
2020-02-03 17:38:37 +0800

    test some code

--- src/components/wrapper.vue
+++ src/components/wrapper.vue
@@ -1,15 +1,20 @@
 <script lang="tsx">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, reactive } from 'vue'
 
 const wrapper = defineComponent({
   setup(props, { slots }) {
     const v = ref(0)
+    const state = reactive({
+      count: 0
+    })
     return () => (
       <div>
         一段文字 {slots.default && slots.default()}
-        <button onClick={() => v.value++}> + </button>
+        <button onClick={() => {v.value++; state.count++}}> + </button>
       	<h1 class="ok">
-      		标题{v.value}
+          标题{v.value}
+          <br/>
+          数目： {state.count}
         </h1>
       </div>
     )
